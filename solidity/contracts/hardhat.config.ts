f626dc3e57db0ad50c941d40f8d4d4583404af11
Siddharth Sharma <ssiddharth2010@icloud.com>
2023-04-10 17:29:04 -0400

    setup initial hardhat project

---
+++ solidity/contracts/hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.18",
+};
+
+task("accounts", "prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+export default config;
